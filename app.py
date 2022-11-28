d959b1defe4fe0167a64d4f67efaf035a59e49f5
nikki2107 <71547789+nikki2107@users.noreply.github.com>
2022-11-28 19:01:16 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask,jsonify,request;
+from prediction import getpredictions;
+app= Flask(__name__);
+@app.route("/predict-alphabet",methods=['POST'])
+
+def predictdata():
+    image=request.files.get('alphabet');
+    prediction=get_prediction(image);
+    return jsonify({
+        'prediction' : prediction
+    }),200
+    
+if __name__ == '__main__':
+    app.run(debug=True);
+    
+        
